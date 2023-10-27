71cf183f7df208f48c9d5f917d310285214cbf30
Ahmad Fauzan <ahmad.fauzan@batmandiri.com>
2023-10-27 21:35:15 +0700

    update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../Assets/logoGdf.png'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
+import portoPPT from '../Assets/CompanyProfile_PTGhandafi.pdf'
 function Hero() {
   return (
     <div className="bg-[#0F0F0F] border-t border-[#AFBD77] ">
@@ -11,7 +12,9 @@ function Hero() {
           </p>
           <p className="text-[16px] italic desktop3:pt-3 desktop2:pt-2 desktop:pt-1 phone:text-center tablet:text-center laptop:text-center text-[#AFBD77]">PT. Ghandafi Berkah Utama</p>
           <button className="mt-4 border border-[#AFBD77] items-center flex justify-center text-[14px]  gap-2  text-[#AFBD77] hover:bg-[#AFBD77] hover:text-white hover:font-bold rounded-[5px] desktop3:py-2 desktop3:w-[10vw] desktop2:py-2 desktop2:w-[15vw] desktop:py-1 desktop:w-[20vw] phone:mx-auto phone:p-3 tablet:mx-auto tablet:p-3 laptop:mx-auto laptop:p-3">
-            <span className="material-symbols-outlined">description</span>Portofolio Company
+            <a href={portoPPT} download="Portofolio Company PT. Ghandafi.pdf" target="_blank" rel="noreferrer" className="flex items-center gap-3">
+              <span className="material-symbols-outlined">description</span>Portofolio Company
+            </a>
           </button>
         </div>
         <LazyLoadImage src={logo} alt="" className="desktop3:w-[30vw] desktop:w-[30vw] desktop2:w-[30vw]  phone:w-[70vw] phone:mx-auto phone:mb-10 phone:mt-5 tablet:w-[50vw] tablet:mx-auto tablet:mb-10 tablet:mt-5 laptop:w-[50vw] laptop:mx-auto laptop:mb-10 laptop:mt-5" />
